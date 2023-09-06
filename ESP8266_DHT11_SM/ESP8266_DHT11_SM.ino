916beaed1412d73292eb58889a9f58a63d81ec72
INOVATRIX <143087722+INOVATRIX@users.noreply.github.com>
2023-09-06 14:09:15 +0530

    Add files via upload

---
+++ ESP8266_DHT11_SM/ESP8266_DHT11_SM.ino
@@ -0,0 +1,27 @@
+#include "DHT.h"
+
+#define DPIN 4        //Pin to connect DHT sensor (GPIO number) D2
+#define DTYPE DHT11   // Define DHT 11 or DHT22 sensor type
+
+DHT dht(DPIN,DTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+  delay(2000);
+  
+  float tc = dht.readTemperature(false);  //Read temperature in C
+  float tf = dht.readTemperature(true);   //Read Temperature in F
+  float hu = dht.readHumidity();          //Read Humidity
+
+  Serial.print("Temp: ");
+  Serial.print(tc);
+  Serial.print(" C, ");
+  Serial.print(tf);
+  Serial.print(" F, Hum: ");
+  Serial.print(hu);
+  Serial.println("%");
+}
